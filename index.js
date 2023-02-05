4678c2463051e9d2dcc687929f76110577304eef
Dixit052 <20bsm008@iiitdmj.ac.in>
2023-02-05 21:39:46 +0530

    update port

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(5000, () =>
+const server = app.listen(`${process.env.PORT||5000}`, () =>
   console.log(`Server started on ${process.env.PORT||5000}`)
 );
 const io = socket(server, {
